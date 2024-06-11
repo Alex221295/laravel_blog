bd8233ae2936ed41d5656705eeb10205bc94cb9c
alex <TKDLexa3@meta.ua>
2024-06-11 21:00:39 +0200

    added wysiwyg

--- src/resources/views/admin/post/create.blade.php
+++ src/resources/views/admin/post/create.blade.php
@@ -32,23 +32,27 @@
             <div class="container-fluid">
                 <!-- Small boxes (Stat box) -->
                 <div class="row">
-                    <div class="col-4">
+                    <div class="col-12">
                             <form method="post" action="{{route('admin.post.store')}}">
                                 @csrf
-                                <input type="text" name="title" class="form-control w-400" placeholder="Add title"
-                                       aria-label="Add post">
+                                <input type="text" name="title" class="form-control w-25" placeholder="Add title"
+                                       aria-label="Add post" value="{{old('title')}}">
                                 @error('title')
                                 <div class="text-danger">
                                 {{$message}}
                                 </div>
                                 @enderror
-                                <input type="text" name="content" class="form-control mt-4 w-400" placeholder="Add content"
-                                       aria-label="Add post">
+                                <div class="form-group mt-4" >
+                                <textarea id="summernote" class="form-control  w-400"  placeholder="Add content"
+                                          aria-label="Add post" name="content">
+                                    {{old('content')}}
+                                </textarea>
+                                </div>
+
                                 @error('content')
                                 <div class="text-danger">
                                 {{$message}}
                                 </div>
-                                    <textarea id="summernote" name="editordata"></textarea>
                                 @enderror
 {{--                                <select class="form-control mt-4 w-400">--}}
 {{--                                    @foreach($getCategory as $categoryForPost)--}}
@@ -60,7 +64,9 @@
 {{--                                {{$message}}--}}
 {{--                                </div>--}}
 {{--                                @enderror--}}
-                                <input type="submit" class="btn btn-block btn-primary mt-4 w-25" value="Add post">
+                                <div class="form-group">
+                                    <input type="submit" class="btn btn-block btn-primary mt-4 w-25" value="Add post">
+                                </div>
                             </form>
                     </div>
                     <!-- ./col -->
@@ -71,8 +77,3 @@
         <!-- /.content -->
     </div>
 @endsection
-<script>
-    $(document).ready(function() {
-        $('#summernote').summernote();
-    });
-</script>
