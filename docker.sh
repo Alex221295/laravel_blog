e0666a1a61a24dcbf0d3d2319ddf3371b8999ee6
alex <TKDLexa3@meta.ua>
2024-07-29 18:03:55 +0200

    middleware admin

--- docker.sh
+++ docker.sh
@@ -2,3 +2,7 @@ docker-compose build
 docker-compose up -d
 sudo docker-compose run --rm composer create-project laravel/laravel:^10.0 .
 sudo chmod -R 777 src/
+sudo docker-compose run --rm artisan ui bootstrap
+sudo apt install npm
+npm install
+npm run dev
--- src/routes/web.php
+++ src/routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'Main'], function () {
     Route::get('/', [IndexController::class, 'index'])->name('index');
 });
-Route::group(['namespace' => 'Admin', 'prefix' => 'admin'], function () {
+Route::group(['namespace' => 'Admin', 'prefix' => 'admin','middleware'=>['auth','admin']], function () {
     Route::group(['namespace' => 'Main'], function () {
         Route::get('/', [AdminIndexController::class, 'index'])->name('admin.index');
     });
